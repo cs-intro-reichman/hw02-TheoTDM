9b3284d06b75f8853c55300c56d2b61cda2dba7b
TheoTDM <theodore.daimaman@post.runi.ac.il>
2023-12-29 11:52:17 +0200

    11:52

--- OneOfEachStats.java
+++ OneOfEachStats.java
@@ -12,9 +12,14 @@ public class OneOfEachStats {
 		// Gets the two command-line arguments
 		int t = Integer.parseInt(args[0]);
 		int seed = Integer.parseInt(args[1]);
+		double c = 0.0;
 		// Initailizes a random numbers generator with the given seed value
         Random generator = new Random(seed);  
 		int count = t;
+		int[] familyCount = new int[20];
+		for (int i = 0; i < 20; i++){
+			familyCount[i] = 0;
+		}
 		int f2 = 0;
 		int f3 = 0;
 		int f4 = 0;
@@ -24,39 +29,38 @@ public class OneOfEachStats {
 			int sum = 0;
 			while (!b || !g){
 				sum += 1;
-				double c = generator.nextDouble() * 2;
-				if (c == 0){
+				c = generator.nextDouble();
+				if (c >= 0.5){
 					g = true;
 				}
-				else {
-					b =true;
-			
+				else if (c < 0.5){
+					b = true;
+			 
 				}
 			}
-			if (sum == 2){
-				f2 += 1;
-			}
-			else if (sum == 3){
-				f3 += 1;
-			}
-			else {
-				f4 +=1;
-			}
+			familyCount[sum] += 1;
 			count -= 1;
 		}
-		//double avg = ((2 * f2) + (3 * f3) + (4 * f4)) / (double)t;
-		double avg = (2 * f2 + 3 * f3 + 4 * f4) / (double) (f2 + f3 + f4);
+		int sumOffamily = 0;
+		int moreThen4 = 0;
+		for (int i = 2; i < 20; i++){
+			sumOffamily = sumOffamily + familyCount[i] * i;
+			if (i > 4){
+				moreThen4 += 1;
+			}
+		}
+		double avg = sumOffamily / (double) t;
 		String mode = "4 or more";
-		if (f2 >= f3 && f2 >= f4){
+		if (familyCount[2] >= familyCount[3] && familyCount[2] >= (familyCount[4] + moreThen4)){
 			mode = "2"; 
 		} 
-		else if (f3 >= f4){
+		else if (familyCount[3] >= (familyCount[4] + moreThen4)){
 			mode = "3";
 		}
 		System.out.println("Average: " + avg + " children to get at least one of each gender.");
-		System.out.println("Number of families with 2 children: " + f2);
-		System.out.println("Number of families with 3 children: " + f3);
-		System.out.println("Number of families with 4 or more children: " + f4);
+		System.out.println("Number of families with 2 children: " + familyCount[2]);
+		System.out.println("Number of families with 3 children: " + familyCount[3]);
+		System.out.println("Number of families with 4 or more children: " + familyCount[4]);
 		System.out.println("The most common number of children is " + mode + ".");
 
 		//// In the previous version of this program, you used a statement like:
